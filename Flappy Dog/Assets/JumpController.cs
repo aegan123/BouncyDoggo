449fa5a862b6505c2e92345290508e9c5e06d43b
Niki <niittymaki.niki@gmail.com>
2018-11-07 19:28:44 +0200

    Dog animations added
    Dog animations added, but not implemented properly

---
+++ Flappy Dog/Assets/JumpController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour {
+
+	 
+    Animator anim;
+ 
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Active");
+        }
+    }
+}
