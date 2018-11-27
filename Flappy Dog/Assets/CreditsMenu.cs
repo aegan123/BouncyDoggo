7344d2db13b7dabb566852c8e779fba6cce4ed51
TeemuTenkanen <39051953+TeemuTenkanen@users.noreply.github.com>
2018-11-27 15:37:33 +0200

    added the credits menu
    added the credits menu

---
+++ Flappy Dog/Assets/CreditsMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenu : MonoBehaviour {
+
+
+    public static bool inCredits = false;
+
+    public GameObject CreditsMenuUI;
+
+	public void openCredits()
+    {
+        CreditsMenuUI.SetActive(true);
+        inCredits = true;
+    }
+
+    public void back()
+    {
+        CreditsMenuUI.SetActive(false);
+        inCredits = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (inCredits)
+            {
+                openCredits();
+            }
+            else
+            {
+                back();
+            }
+        }
+    }
+}
